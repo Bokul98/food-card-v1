397812f5466dabc64315808e32ddd52a49454b10
Bokul98 <bokulsorkar96@gmail.com>
2025-08-22 16:35:53 +0600

    update code

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { motion } from 'framer-motion';
 
 export default function NotFound() {
   const router = useRouter();
